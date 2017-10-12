e00279fb5fa817eccfeab96847d7aa2b260ce8b3
Ravi Basa <basa.ravikiran@gmail.com>
2017-10-13 00:00:28 +0300

    updated regex support

--- Hubssh.robot
+++ Hubssh.robot
@@ -1,13 +1,19 @@
 *** Settings ***
-#Library     SSHLibrary
-Library     LogfileAnalysis.py     192.168.56.102    root   None
+Library     SSHLibrary
+Library     LogfileAnalysis     192.168.56.101    root   None
 Test Teardown   close ssh connection
+#Library     UserSSHLibrary.py
 *** Variables ***
-${url}      192.168.56.102
+${url}      192.168.56.101
 ${user}     root
 ${passwd}   ${None}
 
 *** Test Cases ***
 
-Connect SSH via LogfileAnalyzer
-    syslog contains text messgaes   /var/log/messages   50   Input
+#Connect SSH via LogfileAnalyzer
+#    syslog contains text messgaes   /var/log/messages   50   Input
+
+Test Call
+    #syslog contains text message   /logs/syslog   50    Input
+    syslog match regex          /logs/syslog   50
+    ...   data 641 - -\\s\\s0#6#[[]main[]] ReqQueue.hpp[(]56[)]: Requestor
--- LogfileAnalysis.py
+++ LogfileAnalysis.py
@@ -1,23 +1,39 @@
 from SSHLibrary.library import SSHLibrary
 from robot.api  import logger
+from robot.libraries.BuiltIn import BuiltIn
 
 
 class LogfileAnalysis(SSHLibrary):
     def __init__(self,url,login,passwd):
         super(LogfileAnalysis, self).__init__()
+        print type(self)
         self.open_connection(url)
         self.login(login,passwd)
-    def syslog_contains_text_messgaes(self,log_file_path,tail,messages):
+
+    def syslog_contains_text_message(self,log_file_path,tail,message):
         cmd='tail -'+str(tail)+' '+log_file_path
         self.start_command(cmd)
         output=self.read_command_output()
         logger.console(output)
+        BuiltIn().should_contain(output,message)
+
+    def syslog_match_regex(self,log_file_path,tail,regex):
+        cmd='tail -'+str(tail)+' '+log_file_path
+        self.start_command(cmd)
+        output=self.read_command_output()
+        logger.console(output)
+        logger.console(regex)
+        BuiltIn().should_match_regexp(output,regex)
+
+
+
     def close_ssh_connection(self):
         self.close_connection()
 
 
 
 if __name__=='__main__':
-    handler=LogfileAnalysis('192.168.56.102','root',None)
-    handler.syslog_contains_text_messgaes('/var/log/messages','50','Input')
+    handler=LogfileAnalysis('192.168.56.101','root',None)
+    handler.syslog_contains_text_message('/logs/syslog','50','data 641')
+    handler.syslog_match_regex('/logs/syslog', '50', 'data\s[0-9][0-9][0-9]')
     handler.close_ssh_connection()
